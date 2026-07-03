3351ee060b001ce0c02140e388bba746d9bcda1e
adonis13514 <adonis13514@gmail.com>
2026-07-03 13:41:58 +0800

    Add IP handler function to api/ip.js

---
+++ api/ip.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+  const ip =
+    req.headers["x-forwarded-for"] ||
+    req.socket.remoteAddress;
+
+  res.status(200).json({
+    ip: ip,
+    time: new Date().toISOString()
+  });
+}
